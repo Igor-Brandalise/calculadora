8fb4af65ad4483b9e6a930b8f98fba11c3288f9c
Igor Brandalise <brandaliseigor123@gmail.com>
2025-04-29 14:48:58 -0300

    operadores

---
+++ src/components/operador.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface OperadorProps {
+  operador: string;
+  setOperador: (value: string) => void;
+}
+
+const Operador: React.FC<OperadorProps> = ({ operador, setOperador }) => {
+  return (
+    <select
+      value={operador}
+      onChange={(e) => setOperador(e.target.value)}
+      className="border-2 border-black p-2 rounded w-full"
+    >
+      <option value="">Selecione um operador</option>
+      <option value="+">+</option>
+      <option value="-">-</option>
+      <option value="*">*</option>
+      <option value="/">/</option>
+      <option value="**">**</option>
+    </select>
+  );
+};
+
+export default Operador;
